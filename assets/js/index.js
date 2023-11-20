d54968d7ce5ab34d3bdf38cf775934aa5586cb09
Viviane-Maria <viviane.m.oliveira.s@gmail.com>
2023-11-20 00:26:10 -0300

    feat: adiciona js no index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
-// js page home here
+// getElementsByClassName retorna uma coleção (os dois botões) e guarda na variável
+const botoes = document.getElementsByClassName("join");
+
+// Adiciona o ouvinte de evento para os dois botões da página percorrendo a coleção
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener("click", () => {
+        // Redireciona para outra página
+        window.location.href = "./login.html";
+    });
+}
